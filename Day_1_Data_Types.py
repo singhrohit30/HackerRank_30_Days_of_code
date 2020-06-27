f63abca6066149768cc637a1f4c67921d9eaa353
Rohit <rhtkmr084@gmail.com>
2020-06-27 15:21:07 +0530

    Day 1: Learning Data Types by Performing some small Operations

---
+++ Day_1_Data_Types.py
@@ -0,0 +1,17 @@
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+new_i = int(input())
+new_d = float(input())
+new_s = input()
+
+
+print(i + new_i)
+print(d + new_d)
+print(s + new_s)
