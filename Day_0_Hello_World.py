da8852d7562526ca959e87c3ffa1dd63e7a30812
Rohit <rhtkmr084@gmail.com>
2020-06-27 15:15:34 +0530

    Day 0: Program to print "Hello World on the terminal"

---
+++ Day_0_Hello_World.py
@@ -0,0 +1,9 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+
+# Print a string literal saying "Hello, World." to stdout.
+
+input_string = input()
+
+print('Hello, World.')
+
+print(input_string)
